73764f14934929bc508d4d2f53e7ab6b91374e51
XeroSWM <wxmonteros@uce.edu.ec>
2026-02-19 09:40:49 -0500

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,20 @@
-output "1_URL_SISTEMA_ALB" { 
-  value = "http://${aws_lb.main.dns_name}" 
+output "url_sistema_alb" { 
+  value       = "http://${aws_lb.main.dns_name}" 
+  description = "COPIA ESTA URL EN TU NAVEGADOR PARA VER EL SISTEMA"
 }
-output "2_Frontend_IP"  { value = aws_instance.frontend.public_ip }
-output "3_Auth_IP"      { value = aws_instance.auth.public_ip }
-output "4_Core_IP"      { value = aws_instance.core.public_ip }
-output "5_Dashboard_IP" { value = aws_instance.dashboard.public_ip }
+
+output "ip_frontend" { 
+  value = aws_instance.frontend.public_ip 
+}
+
+output "ip_auth" { 
+  value = aws_instance.auth.public_ip 
+}
+
+output "ip_core" { 
+  value = aws_instance.core.public_ip 
+}
+
+output "ip_dashboard" { 
+  value = aws_instance.dashboard.public_ip 
+}
