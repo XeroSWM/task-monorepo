9e1b92d67076e4222fc290413061b8c6456a7779
XeroSWM <wxmonteros@uce.edu.ec>
2026-02-19 09:33:41 -0500

    Update rds.tf

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,8 +1,32 @@
+# 1. Security Group para la Base de Datos (RDS)
+resource "aws_security_group" "rds_sg" {
+  name        = "task-rds-sg"
+  description = "Permitir trafico de DB solo desde las instancias EC2"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port       = 5432
+    to_port         = 5432
+    protocol        = "tcp"
+    # ¡EL CAMBIO CLAVE! Ahora permite conexión desde las EC2 puras
+    security_groups = [aws_security_group.ec2_sg.id] 
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# 2. Grupo de Subredes (Ubicamos la BD en las redes privadas por seguridad)
 resource "aws_db_subnet_group" "main" {
   name       = "task-db-subnet-group"
   subnet_ids = module.vpc.private_subnets
 }
 
+# 3. La Instancia de PostgreSQL
 resource "aws_db_instance" "postgres" {
   identifier             = "task-db"
   engine                 = "postgres"
@@ -13,7 +37,10 @@ resource "aws_db_instance" "postgres" {
   password               = var.db_password
   db_subnet_group_name   = aws_db_subnet_group.main.name
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
-  skip_final_snapshot    = true # Importante para poder destruirlo fácil en Academy
+  
+  # Fundamental para AWS Academy: permite destruir la BD sin pedir respaldos
+  skip_final_snapshot    = true 
+  
   publicly_accessible    = false
   db_name                = "taskdb"
 }
